4ccbbeb963838ac41ee7ea77b84e1dedfa31eb0a
Alex Geis <siegxela@gmail.com>
2022-06-21 11:48:20 -0600

    add template import js file and reconfigure webpack settings to bundle and rename the multiple sources

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import "./style.css";
+import print from "./otherFunction.js";
 
 function component() {
 	const element = document.createElement("div");
 	element.textContent = "hello world!";
 	element.classList.add("hello");
+	print();
 	return element;
 }
 document.body.appendChild(component());
