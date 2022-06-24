e7def188bf89c2242876c5536a9950cd2d21d097
Alex Geis <siegxela@gmail.com>
2022-06-24 11:29:28 -0600

    add event listenting to nav link to direct to menu page, add listener to logo to direct back to homepage

--- src/index.js
+++ src/index.js
@@ -4,16 +4,33 @@ import "./styleSCSS.scss";
 import { headerComponent } from "./js/components/header/header";
 import { mainComponent } from "./js/components/mainContent/mainContent";
 import { footerComponent } from "./js/components/footer/footer";
+import { menuComponent } from "./js/components/menuContent/menuContent";
 
 const bodyContent = document.querySelector("#content");
 //homepage components
 const header = headerComponent();
 const mainContent = mainComponent();
 const footer = footerComponent();
+//menu page components
+const menu = menuComponent();
 
 window.onload = () => {
-	bodyContent.append(header, mainContent, footer);
+	renderHomePage();
+	// bodyContent.append(header, mainContent, footer);
 };
+
+//render menu page on click
+const headerNav1 = document.querySelector("#header-nav1");
+headerNav1.addEventListener("click", renderMenuPage);
+function renderMenuPage() {
+	bodyContent.append(header, menu, footer);
+}
+//render home page on logo click
+const headerLogo = document.querySelector("#header-logo");
+headerLogo.addEventListener("click", renderHomePage);
+function renderHomePage() {
+	bodyContent.append(header, mainContent, footer);
+}
 
 const ghpages = require("gh-pages");
 
