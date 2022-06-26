ad16f911840e6edc1ae1cb5a4a3891e5aa5460b7
Alex Geis <siegxela@gmail.com>
2022-06-25 20:55:12 -0600

    finish about section page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { headerComponent } from "./js/components/header/header";
 import { mainComponent } from "./js/components/mainContent/mainContent";
 import { footerComponent } from "./js/components/footer/footer";
 import { menuComponent } from "./js/components/menuContent/menuContent";
+import { aboutComponent } from "./js/components/aboutContent/aboutContent";
 
 const bodyContent = document.querySelector("#content");
 //homepage components
@@ -13,16 +14,24 @@ const mainContent = mainComponent();
 const footer = footerComponent();
 //menu page components
 const menu = menuComponent();
+//about page components
+const about = aboutComponent();
 
 window.onload = () => {
 	renderHomePage();
 	//render menu page on click
 	const headerNav1 = document.querySelector("#headerLeft");
-	console.log(headerNav1);
 	headerNav1.addEventListener("click", renderMenuPage);
 	function renderMenuPage() {
 		bodyContent.innerHTML = "";
 		bodyContent.append(header, menu, footer);
+	}
+	//render about page on click
+	const headerNav4 = document.querySelector("#headerRight");
+	headerNav4.addEventListener("click", renderAboutPage);
+	function renderAboutPage() {
+		bodyContent.innerHTML = "";
+		bodyContent.append(header, about, footer);
 	}
 	//render home page on logo click
 	const headerLogo = document.querySelector("#header-logo");
